b6c6570871fc28ada493698a9d658d31d2e16f8f
Jabor047 <gkkarobia@gmail.com>
2021-10-10 00:09:52 +0300

    Finished the sql tasks

--- sql_task1.sql
+++ sql_task1.sql
@@ -1,5 +1,6 @@
 SELECT
-  person_id, provider_id,
+  person_id,
+  provider_id,
   MIN(procedure_dat) OVER(PARTITION BY provider_id, person_id) AS firstVisit
 FROM
   `bigquery-public-data.cms_synthetic_patient_data_omop.procedure_occurrence`
